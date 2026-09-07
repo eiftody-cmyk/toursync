39e0136d8f63273e173922a3f5fdda4757b7c85b
Edward Iftody <eiftody@gmail.com>
2026-09-07 15:24:05 +0900

    Handle /1/1/* double-path from GYG self-testing tool

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,14 @@ const nextConfig: NextConfig = {
         destination: "/api/1/",
       },
       {
+        source: "/1/1/:path*/",
+        destination: "/api/1/:path*/",
+      },
+      {
+        source: "/1/1/:path*",
+        destination: "/api/1/:path*",
+      },
+      {
         source: "/1/:path*/",
         destination: "/api/1/:path*/",
       },
