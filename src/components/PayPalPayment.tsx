2bb171d376bd1c4c02ca3235db5ab7429e171387
Edward Iftody <eiftody@gmail.com>
2026-09-09 09:48:03 +0900

    Enable Apple Pay and Google Pay in PayPal SDK

--- src/components/PayPalPayment.tsx
+++ src/components/PayPalPayment.tsx
@@ -110,7 +110,7 @@ export function PayPalPayment({
         clientId: paypalClientId,
         currency,
         intent: "capture",
-        "enable-funding": "venmo,paylater",
+        "enable-funding": "venmo,paylater,applepay,googlepay",
       }}
     >
       <div style={{ opacity: processing ? 0.6 : 1, pointerEvents: processing ? "none" : "auto" }}>
